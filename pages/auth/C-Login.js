53860ff2091f8b04ca5ffa74eb5474d314106d4e
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-11-24 14:19:12 -0300

    Update C-Login.js

--- pages/auth/C-Login.js
+++ pages/auth/C-Login.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../lib/supabaseServer";
+import { supabase } from "../../../lib/c-supabaseClient.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST")
