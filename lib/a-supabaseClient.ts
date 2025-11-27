26c9687593b9257cc88ba094a180eab28c4b2bf9
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-11-27 16:57:21 -0300

    Update a-supabaseClient.ts

--- lib/a-supabaseClient.ts
+++ lib/a-supabaseClient.ts
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const ACADEMY_URL = process.env.NEXT_PUBLIC_SUPABASE_ACADEMY_URL;
-const ACADEMY_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ACADEMY_ANON_KEY;
+const ACADEMY_URL = process.env.NEXT_PUBLIC_SUPABASE_AGENCY_URL;
+const ACADEMY_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_AGENCY_ANON_KEY;
 
-if (!ACADEMY_URL || !ACADEMY_ANON_KEY) {
-  throw new Error("Missing Academy Supabase environment variables");
+if (!AGENCY_URL || !AGENCY_ANON_KEY) {
+  throw new Error("Missing Agency Supabase environment variables");
 }
 
-export const supabaseAcademy = createClient(
-  ACADEMY_URL,
-  ACADEMY_ANON_KEY
+export const supabaseAgency = createClient(
+  AGENCY_URL,
+  AGENCY_ANON_KEY
 );
