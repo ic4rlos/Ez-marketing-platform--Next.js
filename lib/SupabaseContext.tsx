d69b1d5767853a895100ffbed706b131db4212a1
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-12-01 17:22:33 -0300

    Update SupabaseContext.tsx

--- lib/SupabaseContext.tsx
+++ lib/SupabaseContext.tsx
@@ -5,12 +5,19 @@ import { supabaseAgency } from "./a-supabaseClient";
 export const SupabaseContext = createContext(supabaseCompany);
 
 export function SupabaseProvider({ children }: any) {
-  const path = typeof window !== "undefined" ? window.location.pathname : "";
+  const path =
+    typeof window !== "undefined" ? window.location.pathname : "";
 
   let client = supabaseCompany;
 
   if (path.startsWith("/a-")) {
     client = supabaseAgency;
+  }
+
+  // expõe no window também
+  if (typeof window !== "undefined") {
+    window.supabaseCompany = supabaseCompany;
+    window.supabaseAgency = supabaseAgency;
   }
 
   return (
