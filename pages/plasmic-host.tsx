992bcf3e2e074e45509cd4a864f1c93d94bd9de5
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-12-08 13:47:04 -0300

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,7 +1,19 @@
-import * as React from 'react';
-import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '@/plasmic-init';
+import * as React from "react";
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "@/plasmic-init";
 
 export default function PlasmicHost() {
-  return PLASMIC && <PlasmicCanvasHost />;
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.PLASMIC_PREVIEW = { user: "debug" };
+          `,
+        }}
+      />
+      <PlasmicCanvasHost />
+    </>
+  );
 }
+
