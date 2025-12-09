b1ee4b58d67b14cb3419aa3c1587234fc6750edc
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-12-09 15:03:29 -0300

    Create plasmic-host-check.tsx

---
+++ pages/plasmic-host-check.tsx
@@ -0,0 +1,8 @@
+export default function Teste() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>SE VOCÊ ESTÁ VENDO ISSO</h1>
+      <p>O VERCEL FINALMENTE ESTÁ SERVINDO O NOVO BUILD</p>
+    </div>
+  );
+}
